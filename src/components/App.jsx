fb3b1508beb3588663338236737789c0fd16561c
Pete Schroeder <pete.schroeder@protonmail.com>
2021-12-09 15:23:06 -0500

    Added
    react

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import '../styles/index.scss';
+import Recipes from './Recipes';
+
+const App = () => {
+  return (
+    <>
+      <section className="hero"></section>
+      <main>
+        <section>
+          <h1>Hello react world!</h1>
+        </section>
+      </main>
+
+      <Recipes />
+    </>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,5 @@
-import './styles/index.scss';
+import { render } from 'react-dom';
 
-const elvenShieldRecipe = {
-  leatherStrips: 2,
-  ironIngot: 1,
-  refinedMoonstone: 4
-}
+import App from './components/App';
 
-const elvenGauntletRecipe = {
-  ...elvenShieldRecipe,
-  leather: 1,
-  refinedMoonstone: 6
-}
-
-console.log(elvenShieldRecipe);
-console.log(elvenGauntletRecipe);
+render(<App />, document.getElementById('root'));
