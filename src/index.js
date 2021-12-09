dd31ec241f29109bea118c2fbadb604c01c29f99
Pete Schroeder <pete.schroeder@protonmail.com>
2021-12-09 13:06:22 -0500

    added babel
    updated scripts

--- src/index.js
+++ src/index.js
@@ -4,4 +4,11 @@ const elvenShieldRecipe = {
   refinedMoonstone: 4
 }
 
+const elvenGauntletRecipe = {
+  ...elvenShieldRecipe,
+  leather: 1,
+  refinedMoonstone: 6
+}
+
 console.log(elvenShieldRecipe);
+console.log(elvenGauntletRecipe);
