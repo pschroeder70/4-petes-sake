424a3cd5ad374348fcc4e23a360e5779ef9595fc
Pete Schroeder <pete.schroeder@protonmail.com>
2021-12-09 13:58:20 -0500

    added sass, sass-loader, css-loader, and all other styles stuff
    Go.

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,27 @@
-let mode = 'development';
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-if(process.env.NODE_ENV !== 'production') {
+let mode = 'development';
+let target = 'web';
+
+if(process.env.NODE_ENV === 'production') {
   mode = 'production';
+  target = 'browserslist';
 }
 
 module.exports = {
   mode: mode,
-
+  target: target,
   module: {
     rules: [
+      {
+        test: /\.s?css$/i,
+        use: [
+          MiniCssExtractPlugin.loader, 
+          'css-loader', 
+          'postcss-loader', 
+          'sass-loader',
+        ],
+      },
       {
         test: /\.js$/,
         exclude: /node_modules/,
@@ -17,9 +30,14 @@ module.exports = {
     ],
   },
 
+  plugins: [
+    new MiniCssExtractPlugin()
+  ],
+
   devtool: 'source-map',
 
   devServer: {
     static: './dist',
+    hot: true,
   },
 };
