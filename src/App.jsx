9382a8be9c8a16909d4f97eff53ab5a82ac06833
Pete Schroeder <71779142+pschroeder70@users.noreply.github.com>
2023-07-19 13:33:11 -0400

    more fixes
    Updates

--- src/App.jsx
+++ src/App.jsx
@@ -16,17 +16,20 @@ const App = () => {
         <Content>
           <h2>What to do?</h2>
 
-          <h3>About me? </h3><span class="section__content--date">7/7/2023</span>
+          <h3>About me? </h3><span className="section__content--date">7/7/2023</span>
           <p>Aww you wanna know about me?</p>
           <p>Well I am an avid movie collector/watcher, mostly 4k.  I know enough about cars to get myself in trouble when I (try) to fix them.</p>
           <p>Been married more then once, have some amazing kids, all grown up now.  And currently the best wife in the world, no...you can't have her.</p>
           <p>Was with my last company for 10 years, and sadly was laid-off, so been looking for my next adventure!</p>
           <p>I also feel like, my site is never 'ready', partially due to not having much artistic skill, and when you do something for a living you never get time to work on your own thing.  Example: Mechanic with a busted ride, or a house painter whos house needs painting, that sort of thing.</p>
+          <script>
+            const canvas = document.getElementById("tutorial");
+            const ctx = canvas.getContext("2d");
+          </script>
         </Content>
         <Stats
           title="Some fun stuff should be over here"
         >
-          This is a work in progress, could you guess?
         </Stats>
       </Section>
       <Footer />
--- src/components/content/index.jsx
+++ src/components/content/index.jsx
@@ -1,9 +1,14 @@
+
+
 const Content = (props) => {
   return (
     <div className='content'>
       {props.children}
+
+      <canvas id="canvas" width="150" height="150">word</canvas>
     </div>
   );
 };
 
 export default Content;
+import './context.js'
