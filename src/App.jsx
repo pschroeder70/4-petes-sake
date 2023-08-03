02480731ed5009908585b0cbfbfa89678cbc80a3
Pete Schroeder <71779142+pschroeder70@users.noreply.github.com>
2023-08-03 12:57:24 -0400

    Push
    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import "./sass/main.scss";
-
 import Header from "./components/Header";
 import Section from "./components/Section";
 import Stats from "./components/SideBar";
 import Footer from "./components/Footer";
-
 import Home from "./components/Pages/Home";
 import HomeTheater from "./components/pages/HomeTheater";
 import GarageBar from "./components/pages/GarageBar";
--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,46 @@
-const path = require("path");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-let mode = "production"; // 'production';
-let target = "web";
+let mode = 'development'; // 'production';
+let target = 'web';
 
 module.exports = {
   mode: mode,
   target: target,
 
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, 'dist'),
   },
 
   module: {
     rules: [
       {
         test: /\.(png|jpe?g|gif|svg|mp4)$/i,
-        type: "asset/resource",
+        type: 'asset/resource',
         generator: {
-          filename: "images/[name][ext][query]",
-        },
+          filename: 'images/[name][ext][query]'
+        }
       },
       {
         test: /\.(s[ac]|c)ss$/i,
-        use: [
-          MiniCssExtractPlugin.loader,
-          "css-loader",
-          "postcss-loader",
-          "sass-loader",
-        ],
+        use: [MiniCssExtractPlugin.loader, 'css-loader','postcss-loader','sass-loader'],
         generator: {
-          filename: "styles/[hash][ext][query]",
-        },
+          filename: 'styles/[hash][ext][query]'
+        }
       },
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loader: "babel-loader",
+        loader: 'babel-loader',
       },
       {
         test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        type: "asset/resource",
+        type: 'asset/resource',
         generator: {
-          filename: "fonts/[name][ext][query]",
-        },
+          filename: 'fonts/[name][ext][query]'
+        }
       },
     ],
   },
@@ -54,22 +49,23 @@ module.exports = {
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin(),
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
+      template: './src/index.html',
     }),
   ],
 
   resolve: {
-    extensions: [".js", ".jsx"],
+    extensions: ['.js', '.jsx'],
   },
 
   performance: {
-    hints: false,
+    hints: false
   },
 
   // devtool: 'source-map',
 
   devServer: {
-    static: "./dist",
+    static: './dist',
     hot: true,
   },
+  
 };
