097c5ae2d477f5af061b2db3fd7e20e33677b097
Pete Schroeder <71779142+pschroeder70@users.noreply.github.com>
2023-08-04 10:46:55 -0400

    small edits
    update colors, small movie card changes.

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,16 +4,16 @@ function MovieCard({ movie }) {
   return (
     <div className='movie-card'>
       <div className='movie-card__top'>
+        <div className='movie-card__top-data'>
+          <p className='movie-card__title'>{movie.title}</p>
+          <p className='movie-card__release'>{movie.release_date}</p>
+        </div>
         <div className='movie-card__top-image'>
           <img
             src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
             alt={movie.title}
             className='movie-card__image'
           />
-        </div>
-        <div className='movie-card__top-data'>
-          <p className='movie-card__title'>{movie.title}</p>
-          <p className='movie-card__release'>{movie.release_date}</p>
         </div>
       </div>
       <div className='movie-card__overview'>{movie.overview}</div>
