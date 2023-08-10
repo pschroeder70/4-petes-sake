c267259516e3ca0da9ad19545d3c2ee47fc94753
Pete Schroeder <71779142+pschroeder70@users.noreply.github.com>
2023-08-10 12:13:20 -0400

    more tweaks

--- src/components/MovieTile.jsx
+++ src/components/MovieTile.jsx
@@ -21,22 +21,21 @@ function MovieTile({ movie }) {
             className='movie-tile__image'
           />
         </button>
-        <dialog ref={dialogRef} className="modal">
-          <div className="movie-tile__modal">
-          <img
-            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-            alt={movie.title}
-            className='movie-tile__image'
-          />
-          <div className='movie-tile__copy'>
-            <p className='movie-tile__copy-title'>{movie.title}</p>
-            <p className='movie-tile__copy-release'>
-              Release Date : {movie.release_date}
-            </p>
-            <div className='movie-tile__copy-overview'>{movie.overview}</div>
-            <button onClick={closeDialog}>Close</button>
-          </div>
-
+        <dialog ref={dialogRef} className='modal'>
+          <div className='movie-tile__modal'>
+            <img
+              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+              alt={movie.title}
+              className='movie-tile__image'
+            />
+            <div className='movie-tile__copy'>
+              <button onClick={closeDialog}>Close</button>
+              <p className='movie-tile__copy-title'>{movie.title}</p>
+              <p className='movie-tile__copy-release'>
+                Release Date : {movie.release_date}
+              </p>
+              <div className='movie-tile__copy-overview'>{movie.overview}</div>
+            </div>
           </div>
         </dialog>
       </div>
